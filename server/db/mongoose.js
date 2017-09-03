43a614ae06cd3c6215f1c76bd1a32e964ae2858e
Mathias <mathiaswbrejne@outlook.com>
2017-09-03 10:02:49 +0200

    Corrected database userdata

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds121534.mlab.com:21534/secret-harbor-26519' || 'mongodb://localhost:27017/TodoApp', {useMongoClient: true});
+mongoose.connect('mongodb://Mattertorian:Wertis313@ds121534.mlab.com:21534/secret-harbor-26519' || 'mongodb://localhost:27017/TodoApp', {useMongoClient: true});
 
 module.exports = {
     mongoose
