e51a3784d05a26925beec3cdf81c53fccd3dc512
Mathias <mathiaswbrejne@outlook.com>
2017-09-04 17:34:38 +0200

    Add database test with mongoose file

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
+
+
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGO_URI = 'mongodb://Mattertorian:Wertis313@ds121534.mlab.com:21534/secret-harbor-26519', {useMongoClient: true});
+mongoose.connect(process.env.MONGO_URI, {useMongoClient: true});
 
 module.exports = {
     mongoose
