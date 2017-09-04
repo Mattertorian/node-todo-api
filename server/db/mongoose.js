a7523f3d4ac93fd38976399044c3ffc5d816f728
Mathias <mathiaswbrejne@outlook.com>
2017-09-04 17:31:30 +0200

    Add test database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://Mattertorian:Wertis313@ds121534.mlab.com:21534/secret-harbor-26519' || 'mongodb://localhost:27017/TodoApp', {useMongoClient: true});
+mongoose.connect(process.env.MONGO_URI = 'mongodb://Mattertorian:Wertis313@ds121534.mlab.com:21534/secret-harbor-26519', {useMongoClient: true});
 
 module.exports = {
     mongoose
